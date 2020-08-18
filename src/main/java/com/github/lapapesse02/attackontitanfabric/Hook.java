bba6de7a8e8b95583eab1e292c513308ecf579e6
LaPapesse <68287723+LaPapesse02@users.noreply.github.com>
2020-08-18 19:42:44 +0200

    Add files via upload

---
+++ src/main/java/com/github/lapapesse02/attackontitanfabric/Hook.java
@@ -0,0 +1,9 @@
+package com.github.lapapesse02.attackontitanfabric;
+
+import net.minecraft.item.Item;
+
+public class Hook extends Item {
+    public Hook(Settings settings) {
+        super(settings);
+    }
+}
